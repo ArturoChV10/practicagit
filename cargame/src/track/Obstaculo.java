fb4e06b5cca3b0350c75ac0b66044d21524a9371
Arturo Chavarria <chavarriavillareviajorgearturo@gmail.com>
2023-08-18 16:52:49 -0600

    modificacion de obstaculos

---
+++ cargame/src/track/Obstaculo.java
@@ -0,0 +1,15 @@
+package track;
+
+public class Obstaculo {
+
+	private String Tipo;
+	private int length;
+	private int damage;
+	
+	public Obstaculo(String pTipo) {
+		this.Tipo = pTipo;
+		length = 3;
+		damage = 10;
+	}
+
+}
